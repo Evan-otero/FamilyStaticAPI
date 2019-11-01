be88191f2732f1b0f0135b523872fbbd99d1afe2
Evan Otero <Evan.otero19@gmail.com>
2019-11-01 23:52:57 +0000

    initial commit

--- src/main.py
+++ src/main.py
@@ -18,6 +18,43 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 
+class Family:
+
+    def __init__(self, last_name):
+        self.last_name = last_name
+        # example list of members
+        self._members = [{
+            "id": self._generateId(),
+            "first_name": "John"
+        }]
+
+    # read-only: Use this method to generate random members ID's when adding members into the list
+    def _generateId(self):
+        return randint(0, 99999999)
+
+    def add_member(self, member):
+        ## you have to implement this method
+        ## append the member to the list of _members
+        pass
+
+    def delete_member(self, id):
+        ## you have to implement this method
+        ## loop the list and delete the member with the given id
+        pass
+
+    def update_member(self, id, member):
+        ## you have to implement this method
+        ## loop the list and replace the memeber with the given id
+        pass
+
+    def get_member(self, id):
+        ## you have to implement this method
+        ## loop all the members and return the one with the given id
+        pass
+
+    def get_all_members(self, id):
+        return self._members
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
